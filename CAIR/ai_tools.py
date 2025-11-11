59f030f2494cd61fb788bf2ec89e9331a98375c5
thoppe <travis.hoppe@gmail.com>
2025-11-11 15:18:35 -0500

    Added new option

--- CAIR/ai_tools.py
+++ CAIR/ai_tools.py
@@ -22,7 +22,8 @@ def chat_with_openai(
     system_prompt: str,
     user_prompt: str,
     reasoning_effort: Gpt5ReasoningEffort,
-        seed:int=None,
+    seed: int = None,
+    cache_expire: int = 60 * 60,
 ):
     """
     Call OpenAI ChatGPT with a chosen model, system prompt, and user prompt.
@@ -31,6 +32,9 @@ def chat_with_openai(
         model (str): The model name, e.g., "gpt-4o-mini" or "gpt-4.1".
         system_prompt (str): The system role instructions.
         user_prompt (str): The user input prompt.
+        reasoning_effort: ["minimal", "low", "medium", "high"]
+        seed (int): Random state for chatgpt
+        cache_expire (int): Pull value from cache, set to None to force.
 
     Returns:
         str: The assistant's reply.
@@ -38,15 +42,17 @@ def chat_with_openai(
 
     # Return a cached answer if possible
     cache_location = Path("cache") / model
-    cache = Cache(cache_location, expire=60 * 60)  # Expire in one hour
-    key = (model, system_prompt, user_prompt, reasoning_effort, seed)
-    if key in cache:
+    cache = Cache(cache_location, expire=cache_expire)
+    key = (model, system_prompt, user_prompt, reasoning_effort)
+    if key in cache and cache_expire is not None:
         return cache[key]
 
     # Make sure we don't have too many input tokens
     system_n = len(encoding.encode(system_prompt))
     prompt_n = len(encoding.encode(user_prompt))
-    assert system_n + prompt_n < 400_000
+    total_tokens = system_n + prompt_n
+    if total_tokens > 400_000:
+        raise ValueError(f"Used {total_tokens} tokens, limit 400,000")
 
     client = OpenAI(api_key=api_key)
 
