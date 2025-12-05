994b3acc02f548ecfa08dadb01b2866651040945
thoppe <travis.hoppe@gmail.com>
2025-12-05 09:33:36 -0500

    Cleaning API

--- CAIR/__init__.py
+++ CAIR/__init__.py
@@ -1,3 +1,13 @@
 from .info import Channel, Video, Search  # noqa
 from .transcribe import Transcription  # noqa
 from .understand import Analyze  # noqa
+from ._version import __version__  # noqa
+
+__all__ = [
+    "Analyze",
+    "Channel",
+    "Search",
+    "Transcription",
+    "Video",
+    "__version__",
+]
