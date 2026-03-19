45840413245bbd9d773bf8d5d2613524d7b22c83
thoppe <travis.hoppe@gmail.com>
2026-03-19 15:47:49 -0400

    Remove outdated Granicus support

--- CAIR/__init__.py
+++ CAIR/__init__.py
@@ -1,6 +1,5 @@
 from .ai_tools import OpenAIResponse
 from .info import Channel, Video, Search, channel_id_from_url
-from .granicus_utils import Granicus, find_viewpublisher_url
 from .s3_utils import s3_location_to_audio_numpy
 from .transcribe import Transcription
 from .understand import Analyze
@@ -9,8 +8,6 @@ from ._version import __version__
 __all__ = [
     "Analyze",
     "Channel",
-    "Granicus",
-    "find_viewpublisher_url",
     "channel_id_from_url",
     "OpenAIResponse",
     "Search",
